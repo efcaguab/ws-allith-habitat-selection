6f3dbdfa967a320cca1ae0ff9eab1fc711966618
Fernando Cagua <efcaguab@gmail.com>
2017-11-14 21:38:27 +1300

    Create directories before moving stuff

--- run_all_windows.sh
+++ run_all_windows.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 make WL=8
-find ./data/processed -type f -print0 | xargs -0 mv -t .data/processed/results_window8/
+mkdir ./data/processed/results_window8/
+find ./data/processed -type f -print0 | xargs -0 mv -t ./data/processed/results_window8/
 make WL=4
-find ./data/processed -type f -print0 | xargs -0 mv -t .data/processed/results_window4/
+mkdir ./data/processed/results_window4/
+find ./data/processed -type f -print0 | xargs -0 mv -t ./data/processed/results_window4/
 make WL=2
-find ./data/processed -type f -print0 | xargs -0 mv -t .data/processed/results_window2/
+./data/processed/results_window2/
+find ./data/processed -type f -print0 | xargs -0 mv -t ./data/processed/results_window2/
