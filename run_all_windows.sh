3776e611284481df0adeeec7b42c4bd476d70fc5
Fernando Cagua <efcaguab@gmail.com>
2017-11-14 21:47:58 +1300

    Ensure it runs smoothly

--- run_all_windows.sh
+++ run_all_windows.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
 make WL=8
-mkdir ./data/processed/results_window8/
-find ./data/processed -type f -print0 | xargs -0 mv -t ./data/processed/results_window8/
-make WL=4
-mkdir ./data/processed/results_window4/
-find ./data/processed -type f -print0 | xargs -0 mv -t ./data/processed/results_window4/
-make WL=2
-./data/processed/results_window2/
-find ./data/processed -type f -print0 | xargs -0 mv -t ./data/processed/results_window2/
+OUT=$?
+if [ $OUT -eq 0 ];then
+  mkdir ./data/processed/results_window8/
+  find ./data/processed -type f -print0 | xargs -0 mv -t ./data/processed/results_window8/
+  make WL=4
+  OUT2=$?
+  if [ $OUT2 -eq 0 ];then
+    mkdir ./data/processed/results_window4/
+    find ./data/processed -type f -print0 | xargs -0 mv -t ./data/processed/results_window4/
+    make WL=2
+    OUT3=$?
+    if [ $OUT3 -eq 0 ];then
+      ./data/processed/results_window2/
+      find ./data/processed -type f -print0 | xargs -0 mv -t ./data/processed/results_window2/
+    fi
+  fi
+fi
+   
